09c7104eb443cbc226aafbfe923a630511a59a29
Glenn <glenn.lee.23@seoulforeign.org>
2021-09-18 23:00:20 +0900

    optimizations
    optimizationsoptimizations

--- datagrab.py
+++ datagrab.py
@@ -4,7 +4,7 @@ import math
 s = requests.Session()
 #list of intl schools: ()
 FEC_key = 'vYPKUmsd6xZNfsOafhXj7Q9mb9jseNtWv7h2PEZJ'
-EmpNoP = 'Seoul Foreign School'
+EmpNoP = 'Cheongna Dalton'
 EmP = EmpNoP.replace(' ', "+")
 Ecycle = '2020' #2019-2020
 
@@ -35,6 +35,13 @@ getByEmployer = s.get(getByEmployerLink, headers={
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
 }, timeout=50)
 
+if "contributor_street_2" not in getByEmployer.text:
+    print("error, either no school or no donations")
+    exit()
+
+else:
+    pass
+
 with open(f'{EmpNoP} - {Ecycle}.json', 'wb') as outf:
     outf.write(getByEmployer.content)
 
@@ -43,6 +50,7 @@ reptotal = []
 EmpJson = getByEmployer.json()
 pages = (EmpJson['pagination']['pages'])
 currPage = 1
+
 for xxloop in range(pages):
     if currPage == 1:
         for i in range(len(EmpJson['results'])):
@@ -59,6 +67,8 @@ for xxloop in range(pages):
             if toParty == 'REP':
                 reptotal.append(donatedAmnt)
             print(f"{donatedDate} - {toParty} - ${donatedAmnt}")
+            with open(f'{EmpNoP} - Donations.txt', 'a') as outf2:
+                outf2.writelines(f"{donatedDate} - {toParty} - ${donatedAmnt}")
         LIndex = (EmpJson['pagination']['last_indexes']['last_index'])
         LDate = (EmpJson['pagination']['last_indexes']['last_contribution_receipt_date'])
         currPage += 1
@@ -83,6 +93,8 @@ for xxloop in range(pages):
             if toParty == 'REP':
                 reptotal.append(donatedAmnt)
             print(f"{donatedDate} - {toParty} - ${donatedAmnt}")
+            with open(f'{EmpNoP} - Donations.txt', 'a') as outf3:
+                outf3.writelines(f"{donatedDate} - {toParty} - ${donatedAmnt}")
         LIndex = (EmpJson2['pagination']['last_indexes']['last_index'])
         LDate = (EmpJson2['pagination']['last_indexes']['last_contribution_receipt_date'])
         currPage += 1
